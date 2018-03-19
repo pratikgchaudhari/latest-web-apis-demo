8cbfeccc22499225baa84ac739b468abddda42ff
Pratik Chaudhari <chaudharipratikg@gmail.com>
2018-03-19 11:47:25 +0530

    Parameterized '--chess-board-spacing' & '--chess-board-square-size'

--- css-paint-api/js/chess-board-painter.js
+++ css-paint-api/js/chess-board-painter.js
@@ -1,13 +1,17 @@
 class ChessBoardPainter {
+    static get inputProperties(){
+        return ['--chess-board-spacing','--chess-board-square-size'];
+    }
     paint(ctx, geom, properties) {
         const colors = ['black', 'white'];
-        const squareSize = 20;
+        const squareSize = parseInt(properties.get('--chess-board-square-size').toString());
+        const squareSpacing = parseInt(properties.get('--chess-board-spacing').toString());
         for (let i = 0; i < Math.round(geom.height); i++) {
             for (let j = 0; j < Math.round(geom.width); j++) {
                 let color = colors[(i + j) % colors.length];
                 ctx.beginPath();
                 ctx.fillStyle = color;
-                ctx.rect(i * squareSize, j * squareSize, squareSize, squareSize);
+                ctx.rect(i * (squareSize+squareSpacing), j * (squareSize+squareSpacing), squareSize, squareSize);
                 ctx.fill();
             }
         }
